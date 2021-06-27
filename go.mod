ce249547c643f0f1094eb605d85963e42ab554d7
Yudi Primaputra <xpartacvs@gmail.com>
2021-06-28 05:52:21 +0700

    feat(packages): Add config package

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module https-doctor
 
 go 1.16
+
+require (
+	github.com/rs/zerolog v1.23.0
+	github.com/spf13/viper v1.8.1
+)
---
+++ packages/config/config.go
@@ -0,0 +1,93 @@
+package config
+
+import (
+	"os"
+	"regexp"
+	"strings"
+	"sync"
+	"unicode"
+
+	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	hosts    []string
+	logLevel zerolog.Level
+}
+
+type Config interface {
+	Hosts() []string
+	ZerologLevel() zerolog.Level
+}
+
+var (
+	cfg  *config
+	once sync.Once
+)
+
+func (c *config) Hosts() []string {
+	return c.hosts
+}
+
+func (c *config) ZerologLevel() zerolog.Level {
+	return c.logLevel
+}
+
+func load() *config {
+	fang := viper.New()
+
+	fang.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	fang.AutomaticEnv()
+
+	fang.SetConfigName("https-doctor")
+	fang.SetConfigType("yml")
+	fang.AddConfigPath(".")
+
+	value, available := os.LookupEnv("CONFIG_LOCATION")
+	if available {
+		fang.AddConfigPath(value)
+	}
+
+	_ = fang.ReadInConfig()
+
+	return &config{
+		hosts:    splitCSV(fang.GetString("hosts")),
+		logLevel: setLogLevel(fang.GetString("loglevel")),
+	}
+}
+
+func splitCSV(s string) []string {
+	strTrimmed := strings.TrimFunc(
+		s,
+		func(r rune) bool {
+			return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+		},
+	)
+	comaToSpace := strings.NewReplacer(",", " ")
+	strReplacedComa := comaToSpace.Replace(strTrimmed)
+	rgxSpaceSplit := regexp.MustCompile(`\s+`)
+	return rgxSpaceSplit.Split(strReplacedComa, -1)
+}
+
+func setLogLevel(l string) zerolog.Level {
+	switch l {
+	case "debug":
+		return zerolog.DebugLevel
+	case "info":
+		return zerolog.InfoLevel
+	case "warn":
+		return zerolog.WarnLevel
+	case "error":
+		return zerolog.ErrorLevel
+	default:
+		return zerolog.Disabled
+	}
+}
+
+func Get() Config {
+	once.Do(func() {
+		cfg = load()
+	})
+	return cfg
+}
