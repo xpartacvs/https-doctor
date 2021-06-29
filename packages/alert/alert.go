12f444edc251f5c3caeec3119687f86b73c5793a
Yudi Primaputra <xpartacvs@gmail.com>
2021-06-29 19:17:19 +0700

    feat(alert): Add logger package

--- packages/alert/alert.go
+++ packages/alert/alert.go
@@ -3,11 +3,13 @@ package alert
 import (
 	"errors"
 
+	"github.com/rs/zerolog"
 	"github.com/xpartacvs/go-dishook"
 )
 
 type alert struct {
 	payload dishook.Payload
+	logger  *zerolog.Logger
 }
 
 type Alert interface {
@@ -16,6 +18,12 @@ type Alert interface {
 	Send(url string, flushAfter bool) error
 	SetBotName(name string) Alert
 	SetBotAvatar(url string) Alert
+	SetZerolog(logger *zerolog.Logger) Alert
+}
+
+func (a *alert) SetZerolog(logger *zerolog.Logger) Alert {
+	a.logger = logger
+	return a
 }
 
 func (a *alert) Send(url string, flushAfter bool) error {
@@ -73,5 +81,6 @@ func New(message string) Alert {
 				},
 			},
 		},
+		logger: nil,
 	}
 }
