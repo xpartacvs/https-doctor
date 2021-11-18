248f09004e20a8026ba20af7c3cf479cfa09f309
Yudi Primaputra <xpartacvs@gmail.com>
2021-11-18 17:29:59 +0700

    Refactor client and worker

--- packages/worker/worker.go
+++ packages/worker/worker.go
@@ -39,8 +39,8 @@ func job() {
 			defer wg.Done()
 			var title, content string = strings.ToUpper(host), ""
 			httpsClient := client.New(host, logger.Log())
-			result, expiry := httpsClient.GetExpiry()
-			switch result {
+			expiry, err := httpsClient.GetExpiry()
+			switch err {
 			case client.ErrTimeout:
 				content = "Connection timeout."
 				if config.Get().ZerologLevel() != zerolog.Disabled {
@@ -55,13 +55,13 @@ func job() {
 				content = "Wrong SSL certificate."
 				notif.AddField(title, content, false)
 			case client.ErrCertExpired:
-				days := int(time.Until(expiry).Hours()/24) * -1
+				days := int(time.Until(*expiry).Hours()/24) * -1
 				content = "SSL expired for " + strconv.Itoa(days) + " days."
 				notif.AddField(title, content, false)
 			default:
 				graceTime := expiry.AddDate(0, 0, config.Get().Graceperiod())
 				if time.Now().After(graceTime) {
-					days := int(time.Until(expiry).Hours() / 24)
+					days := int(time.Until(*expiry).Hours() / 24)
 					content = "SSL expired in " + strconv.Itoa(days) + " days."
 					notif.AddField(title, content, false)
 				}
